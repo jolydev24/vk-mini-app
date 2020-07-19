dd7a322ba6ef61a9732d24c0f068105bbf23c1d8
Phpesher <superduperproger@gmail.com>
2020-07-19 20:21:41 +0300

    Updates

---
+++ precache-manifest.30ba8b1871def397aff24ca640032d6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "20db04a6096dceb76136",
+    "url": "./static/css/main.f6e836ce.chunk.css"
+  },
+  {
+    "revision": "20db04a6096dceb76136",
+    "url": "./static/js/main.04a27b9b.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "101ff3b626897714c4dc",
+    "url": "./static/css/2.92416b59.chunk.css"
+  },
+  {
+    "revision": "101ff3b626897714c4dc",
+    "url": "./static/js/2.eb0c2b69.chunk.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "0aa9e814e48bc477ba7fee1c2bce6d31",
+    "url": "./index.html"
+  }
+];
